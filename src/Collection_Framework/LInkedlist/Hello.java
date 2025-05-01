815675b8063fd637070a18d1a242b0d8e1724de0
krishpatel13072006 <krishpatelhacker.13579@gmail.com>
2025-05-01 14:35:29 +0530

    LinkedList Vector

---
+++ src/Collection_Framework/LInkedlist/Hello.java
@@ -0,0 +1,38 @@
+package Collection_Framework.LInkedlist;
+import java.util.*;
+public class Hello {
+
+    public static void main(String[] args) {
+
+        LinkedList<Integer> obj=new LinkedList<>(Arrays.asList(3,4,5,6,7,8,9));
+        obj.add(10);
+        obj.addFirst(0);
+        obj.add(1,2);
+        obj.addLast(11);
+
+        System.out.println(obj);
+
+        List<Integer> x=List.of(1,2);
+        obj.removeAll(x);
+        System.out.println(obj);
+
+        List<Integer> x1=List.of(4,5,6,7);
+        System.out.println(obj.contains(23));
+        System.out.println(obj.containsAll(x1));
+
+        System.out.println( obj.peekFirst());
+        System.out.println(obj.getLast());
+
+        System.out.println(obj.poll());
+        System.out.println(obj.pollLast());
+        System.out.println(obj.pop());
+
+        for (Integer n: obj)
+        {
+            System.out.println(n);
+        }
+
+
+
+    }
+}
---
+++ src/Collection_Framework/Vector/Helloo.java
@@ -0,0 +1,46 @@
+package Collection_Framework.Vector;
+
+import java.util.*;
+
+public class Helloo {
+
+    public static void main(String[] args) {
+        Vector<Integer> obj= new Vector<>(10,3);
+
+
+        obj.addFirst(1);
+        obj.addLast(4);
+        System.out.println(obj);
+        obj.add(1,3);
+        obj.add(0,0);
+        System.out.println(obj);
+
+        obj.removeAll(Arrays.asList(0,1));
+
+
+        System.out.println(obj.reversed());
+
+        System.out.println(obj.isEmpty());
+
+        obj.set(0,1);
+        System.out.println(obj);
+
+        for(int x: obj)
+        {
+            System.out.println(x);
+        }
+
+        System.out.println(obj.contains(1));
+        System.out.println(obj.containsAll(List.of(1,4)));
+
+        Collections.sort(obj.reversed());
+        System.out.println(obj);
+
+        obj.clear();
+        System.out.println(obj);
+
+
+
+
+    }
+}
